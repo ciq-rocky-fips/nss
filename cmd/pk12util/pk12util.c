5c9395a437399d256993dd0a8cd7b15e979cc703
Jeremy Allison <jallison@ciq.com>
2025-10-10 14:11:46 -0700

    Apply nss-3.79-pkcs12-fips-defaults.patch: PKCS12 FIPS defaults
    This patch applies changes from the RHEL/CentOS/Fedora NSS package.
    Source: nss-3.79-pkcs12-fips-defaults.patch
    
    PKCS12 FIPS defaults

--- cmd/pk12util/pk12util.c
+++ cmd/pk12util/pk12util.c
@@ -1150,6 +1150,11 @@ main(int argc, char **argv)
         goto done;
     }
 
+    if (PK11_IsFIPS()) {
+        cipher =  SEC_OID_AES_256_CBC;
+        certCipher =  SEC_OID_AES_128_CBC;
+    }
+
     if (pk12util.options[opt_Cipher].activated) {
         char *cipherString = pk12util.options[opt_Cipher].arg;
 
@@ -1164,9 +1169,6 @@ main(int argc, char **argv)
         }
     }
 
-    if (PK11_IsFIPS()) {
-        certCipher =  SEC_OID_UNKNOWN;
-    }
     if (pk12util.options[opt_CertCipher].activated) {
         char *cipherString = pk12util.options[opt_CertCipher].arg;
 
